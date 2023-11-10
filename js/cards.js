d7528b92bdbc5dcab33c078dea927f6b3ddb35db
Kristina <kiegorova04@mail.ru>
2023-11-10 14:43:03 +0300

    award page

--- js/cards.js
+++ js/cards.js
@@ -1,14 +1,33 @@
+const cards = document.querySelectorAll('.card');
 
-// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+cards.forEach(card => {
+  card.addEventListener('click', (e) => {
+    
+    const sectionCards = document.querySelector('.section__cards');
+    const description = document.querySelector('.description');
+    const bg = document.querySelector('.header__bg');
+    
+    sectionCards.classList.add('none');
+    description.classList.remove('none');
+    description.style.backgroundImage += 'url(' + `${e.target.src}` + ')';
+    bg.style.backgroundColor = 'transparent'
+    document.querySelectorAll('.nav__list-a').forEach((el) => {
+    el.style.color = 'white'
+    }) 
 
-var grid = document.querySelector('.grid');
-var msnry;
+    document.querySelector('.prev-descr').addEventListener('click', () => {
+      sectionCards.classList.remove('none');
+      description.classList.add('none');
+  
+      bg.style.backgroundColor += 'var(--gray)'
+  
+      document.querySelectorAll('.nav__list-a').forEach((el) => {
+        el.style.color = 'black'
+      })
+    })
 
-imagesLoaded( grid, function() {
-  // init Isotope after all images have loaded
-  msnry = new Masonry( grid, {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
+  })
+  
+  
 });
+
